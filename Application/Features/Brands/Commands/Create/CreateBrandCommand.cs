b5b208a5d74445d1143c968d18d5c9703967c66f
halitkalayci <halit.kalayci@ext.etiya.com>
2024-07-12 09:49:19 +0300

    Add Create Command

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Application.Repositories;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    // Unit => Fonksiyonda void ne ise request'de o.
+    public class CreateBrandCommand : IRequest<Unit>
+    {
+        // Komutun işlevini yerine getirmesi için alması gereken argümanlar.
+
+        public string Name { get; set; }
+
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, Unit>
+        {
+            // Gerekli bağımlılıklar
+
+            private readonly IBrandRepository _brandRepository;
+
+            public CreateBrandCommandHandler(IBrandRepository brandRepository)
+            {
+                _brandRepository = brandRepository;
+            }
+
+            public Task<Unit> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                // İlgili request ile istediğimiz işlemi yapabiliriz.
+                Brand brand = new Brand()
+                {
+                    Name = request.Name,
+                };
+
+                _brandRepository.Add(brand);
+                return (Task<Unit>)Task.CompletedTask;
+            }
+        }
+    }
+}
