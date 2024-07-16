1aa37a8a6486c8acd78ceea6a874e2087dbae4b7
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-16 11:28:45 +0300

    add register command

---
+++ Application/Features/Auth/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Commands.Register
+{
+    public class RegisterCommand : IRequest<RegisterResponse>
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisterResponse>
+        {
+            // Dep.
+            private readonly IMapper _mapper;
+
+            public Task<RegisterResponse> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+            }
+        }
+    }
+}
