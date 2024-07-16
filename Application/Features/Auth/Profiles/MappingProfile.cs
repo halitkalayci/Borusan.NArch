e766ece3d7dc74f44e766d916439ebda54cdea21
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-16 11:53:28 +0300

    add mapping profile

---
+++ Application/Features/Auth/Profiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Auth.Commands.Register;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterCommand, User>().ReverseMap();
+        }
+    }
+}
