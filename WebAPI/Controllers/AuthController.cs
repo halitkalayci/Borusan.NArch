3290597a1f2d4df33fa6383dcb5d3403544f2dde
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-07-16 11:44:49 +0300

    add auth controller

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Auth.Commands.Register;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterCommand registerCommand)
+        {
+            var response = await _mediator.Send(registerCommand);
+            return Created("", response);
+        }
+    }
+}
